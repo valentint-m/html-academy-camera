257a39c128c9a9331381be8c19e8fec00466275a
Valentin Tazhetdinov <valentyntm@gmail.com>
2025-04-21 12:17:12 +0300

    Добавит вывод ссылок на камеры в форму ввода.

---
+++ src/components/search-form-item/search-form-item.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import { CameraInfo } from '../../types/camera';
+import { getCameraPathById } from '../../utils/utils';
+
+type SearchFormItemProps = {
+  camera: CameraInfo;
+}
+
+export default function SearchFormItem ({camera}: SearchFormItemProps): JSX.Element {
+  return (
+    <li className="form-search__select-item" tabIndex={0}>
+      <Link to={getCameraPathById(camera.id)}>{camera.name}</Link>
+    </li>
+  );
+}
+
--- src/components/search-form/search-form.tsx
+++ src/components/search-form/search-form.tsx
@@ -1,22 +1,36 @@
+import { ChangeEvent, useState } from 'react';
+import { useAppSelector } from '../../hooks';
+import { getCameras } from '../../store/camera-data/camera-data-selectors';
+import SearchFormItem from '../search-form-item/search-form-item';
+
 export default function SearchForm (): JSX.Element {
+  const [searchText, setSearchText] = useState<string>('');
+
+  const cameras = useAppSelector(getCameras);
+
+  function handleSearchInputChange (evt: ChangeEvent<HTMLInputElement>) {
+    const text = evt.target.value;
+    setSearchText(text);
+  }
+
   return (
-    <div className="form-search">
+    <div className={searchText.length >= 3 ? 'form-search list-opened' : 'form-search'}>
       <form>
         <label>
           <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
             <use xlinkHref="#icon-lens"></use>
           </svg>
-          <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" />
+          <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" value={searchText} onChange={handleSearchInputChange} />
         </label>
         <ul className="form-search__select-list">
-          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 8i</li>
-          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 7i</li>
-          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 6i</li>
-          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 5i</li>
-          <li className="form-search__select-item" tabIndex={0}>Cannonball Pro MX 4i</li>
+          {cameras.map((camera) => {
+            if (camera.name.toLowerCase().includes(searchText.toLowerCase())) {
+              return <SearchFormItem camera={camera} key={camera.id} />;
+            }
+          })}
         </ul>
       </form>
-      <button className="form-search__reset" type="reset">
+      <button className="form-search__reset" style={searchText.length > 0 ? {display: 'block'} : {display: 'none'}} type="reset">
         <svg width="10" height="10" aria-hidden="true">
           <use xlinkHref="#icon-close"></use>
         </svg><span className="visually-hidden">Сбросить поиск</span>
